efdff730873ccda8219caf3e730c294e883da6a6
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2023-10-16 23:06:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,10 @@ def urlEncodeString(input_string):
 
 for x in os.walk('.'):
     if x[0] != '.' and ".git" not in x[0]:
-        folder = urlEncodeString(x[0][2:])
-        print(f'added {folder}')
-        data[folder] = {
+        title = x[0][2:]
+        folder = urlEncodeString(title)
+        print(f'added {title}')
+        data[title] = {
                 "file":f'https://fluxusthemes.github.io/ThemeFiles/{folder}/theme.flux',
                 "preview":f'https://fluxusthemes.github.io/ThemeFiles/{folder}/preview.png'
             }
