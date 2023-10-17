c0e030f3a8ae73300db519ada6f64b70648b35b1
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2023-10-16 19:19:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ data = {}
 for x in os.walk('.'):
     if x[0] != '.' and ".git" not in x[0]:
         folder = x[0][2:]
+        print(f'added {folder}')
         data[folder] = {
                 "file":f'https://fluxusthemes.github.io/ThemeFiles/{folder}/theme.flux',
                 "preview":f'https://fluxusthemes.github.io/ThemeFiles/{folder}/preview.png'
